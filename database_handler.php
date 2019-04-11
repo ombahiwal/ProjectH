029a112da43427edc4286068053d288edc88a8ce
ombahiwal <ombahiwal@gmail.com>
2019-04-11 20:58:27 +0530

    Update database_handler.php

--- database_handler.php
+++ database_handler.php
@@ -1,7 +1,7 @@
 <?php 
 //Databse Handler for ProjectH
 //fashionDB is the database name.
-$id="root";$pass="root"; $db="fashiondb"; $conn=mysqli_connect('hetvidb.cwpksjenuvm5.ap-south-1.rds.amazonaws.com',$id,$pass,$db);
+$id="hetvidb";$pass="omkarhetvi"; $db="fashiondb"; $conn=mysqli_connect('hetvidb.cwpksjenuvm5.ap-south-1.rds.amazonaws.com',$id,$pass,$db);
 /*if($conn){
     echo"Database Connected!!<br>";
 }else{
